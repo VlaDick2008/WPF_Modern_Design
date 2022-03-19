a753894413d469c621563a3c76f99e257b25dd29
Vladislav Petrov <sochukushat@yandex.ru>
2022-03-19 12:45:13 +0300

    Залил прикол

---
+++ IPApp/MVVM/ViewModel/DiscoveryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using IPApp.Core;
+
+namespace IPApp.MVVM.ViewModel
+{
+    internal class DiscoveryViewModel : ObservableObject
+    {
+        public DiscoveryViewModel()
+        {
+
+        }
+    }
+}
